c206fe5a48d05c33ce3042d38663290be876b849
Palmer Cox <palmer@fiscalnote.com>
2021-12-11 19:51:40 -0500

    wip-py-bindings

--- bindings/py/build-all.sh
+++ bindings/py/build-all.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-#docker run --rm -it -v $(pwd):/io konstin2/maturin:latest build --release --target x86_64-unknown-linux-gnu --manylinux 2010
-#
-#docker run --rm -it -v $(pwd):/io --entrypoint "" konstin2/maturin:latest /io/build-scripts/build-i686-unkown-linux-gnu.sh
+set -e
 
-docker run --rm -it -v $(pwd):/io maturin-cross build --release --target armv7-unknown-linux-gnueabihf --manylinux 2_24
+docker run --rm -it -v $(pwd):/io konstin2/maturin:latest build --release --target x86_64-unknown-linux-gnu --manylinux 2010
 
-docker run --rm -it -v $(pwd):/io maturin-cross build --release --target aarch64-unknown-linux-gnu --manylinux 2_24
+docker run --rm -it -v $(pwd):/io --entrypoint "" konstin2/maturin:latest /io/build-scripts/build-i686-unkown-linux-gnu.sh
+
+docker run --rm -it -v $(pwd):/io quay.io/dagenix/docker-maturin-cross build --release --target armv7-unknown-linux-gnueabihf --manylinux 2_24
+
+docker run --rm -it -v $(pwd):/io quay.io/dagenix/docker-maturin-cross build --release --target aarch64-unknown-linux-gnu --manylinux 2_24
